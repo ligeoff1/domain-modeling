4718692916882da70d7e52420728f614c8e66c80
ligeoff1 <geoff.li@kellogg.northwestern.edu>
2022-01-20 02:23:49 +0000

    Table creation crm

--- crm/crm.sql
+++ crm/crm.sql
@@ -3,3 +3,43 @@
 -- e.g. DROP TABLE IF EXISTS ______;
 
 -- CREATE TABLES
+
+CREATE TABLE contacts (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  email TEXT,
+  phone_number TEXT,
+  sales INTEGER 
+);
+
+CREATE TABLE interactions (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  contacts_id INTEGER,
+  salespeople_id INTEGER,
+  type TEXT,
+  timestamp TEXT,
+  notes TEXT
+);
+
+CREATE TABLE companies (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  contacts_id INTEGER,
+  industry_id INTEGER,
+  name TEXT
+);
+
+CREATE TABLE industries (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT
+
+);
+
+CREATE TABLE salespeople (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  email TEXT
+);
+
+
