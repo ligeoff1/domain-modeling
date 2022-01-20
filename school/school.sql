7498ab58cd088e2e94ead5a3b74765849da53712
ligeoff1 <geoff.li@kellogg.northwestern.edu>
2022-01-20 01:54:53 +0000

    creation of teacher, section, courses, enrollment tbles

--- school/school.sql
+++ school/school.sql
@@ -16,12 +16,29 @@ CREATE TABLE students (
 
 -- Create the rest of the tables
 
+CREATE TABLE teachers (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT, 
+  last_name TEXT,
+  bio TEXT
+); 
+
+CREATE TABLE courses (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT, 
+  description TEXT
+); 
+
+CREATE TABLE sections (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  time TEXT, 
+  course_id TEXT,
+  teacher_id INTEGER
+); 
+
 CREATE TABLE enrollments (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   student_id INTEGER, 
-  seats INTEGER,
-  semester TEXT,
-  time TEXT,
-  section_number TEXT
+  section_id INTEGER
 );
 
